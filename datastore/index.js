5a17096c03b368370e5aecd9ac66fd222f076b3b
Benjamin Cope <brc1379@gmail.com>
2022-03-15 10:45:40 -0700

    COmplete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -101,14 +101,25 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+
+  fs.unlink(path.join(exports.dataDir, id.toString() + '.txt'), (err) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null);
+    }
+  });
+
+
+
+  // var item = items[id];
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
